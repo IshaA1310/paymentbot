77086828cc49f18c5a7fe4d92e36e5170a52e90a
Isha <isha@valuepitch.com>
2026-01-05 11:29:51 +0530

    updated

--- backend/src/config/razorpay.js
+++ backend/src/config/razorpay.js
@@ -1,10 +1,11 @@
 import Razorpay from "razorpay";
-
+import dotenv from "dotenv";
+dotenv.config();
 const razorpay = new Razorpay({
-    key_id: 'rzp_test_RvM88LQwwarZ7F',
-    key_secret: '40rFmMWBESnZWRMngYGBxdrp'
-    // key_id: process.env.RAZORPAY_KEY_ID,
-    // key_secret: process.env.RAZORPAY_KEY_SECRET
+    // key_id: 'rzp_test_RvM88LQwwarZ7F',
+    // key_secret: '40rFmMWBESnZWRMngYGBxdrp'
+    key_id: process.env.RAZORPAY_KEY_ID,
+    key_secret: process.env.RAZORPAY_KEY_SECRET
 });
 
 export default razorpay;
